995156388103f0b45b9d6bf14f357fdcb8e4bba5
chamale-rac <samuel.chamale23@gmail.com>
2023-08-16 19:51:29 -0600

    feat: gouraud on app and out

--- src/app.py
+++ src/app.py
@@ -9,6 +9,20 @@ out_dir = "../out"
 width = 1920
 height = 1080
 
+gouraud_camouflage = Model(f"{models_dir}/elephant.obj",
+                           translate=(-0.4, -1, -5), scale=(0.09, 0.09, 0.09), rotate=(0, -60, 0))
+gouraud_camouflage.load_texture(f"{textures_dir}/elephant.bmp")
+gouraud_camouflage.set_shaders(
+    shaders.vertex_shader, shaders.gouraud_shader)
+renderer = Renderer(width, height)
+renderer.set_clear_color(32/255, 33/255, 36/255)
+renderer.clear()
+renderer.look_at(cam_pos=(1, 2, 0), eye_pos=(0, 0, -5), rotateZ=0)
+renderer.add_model(gouraud_camouflage)
+renderer.gl_render()
+renderer.gl_finish(f"{out_dir}/gouraud.bmp")
+
+
 elephant_camouflage = Model(f"{models_dir}/elephant.obj",
                             translate=(-0.4, -1, -5), scale=(0.09, 0.09, 0.09), rotate=(0, -60, 0))
 elephant_camouflage.load_texture(f"{textures_dir}/elephant.bmp")
