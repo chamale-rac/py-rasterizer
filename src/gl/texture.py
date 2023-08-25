53af4e6b676a3f83c636536dc204f04029ea639f
chamale-rac <samuel.chamale23@gmail.com>
2023-08-25 12:20:43 -0600

    feat: final render

--- src/gl/texture.py
+++ src/gl/texture.py
@@ -27,6 +27,8 @@ class Texture:
         Returns:
             The color of the texture at the given coordinates if they lie within the texture boundaries, otherwise None.
         """
-        x = int(u * self.width)
-        y = int(v * self.height)
-        return self.pixels[y][x]
+        if 0 <= u < 1 and 0 <= v < 1:
+            x = int(u * self.width)
+            y = int(v * self.height)
+            return self.pixels[y][x]
+        return (0, 0, 0)
